0fa78b5f0eb27b6e4296ea18375ee689c046f9bd
zahids4 <saimzahid@me.com>
2020-06-04 15:06:13 -0400

    Initial Commit

---
+++ SwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Saim Zahid on 2020-06-04.
+//  Copyright © 2020 Saim Zahid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
