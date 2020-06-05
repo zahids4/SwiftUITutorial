27f55ad16e4c3a32f6c622e0bb16c55374a20cd2
zahids4 <saimzahid@me.com>
2020-06-05 13:28:55 -0400

    Using AnyView and addings docs about @ViewBuilder

--- SwiftUITutorial/Views/StoryRow.swift
+++ SwiftUITutorial/Views/StoryRow.swift
@@ -13,15 +13,39 @@ struct StoryRow: View {
     var titleColor: Color = Color.white
     var descriptionColor: Color = Color.white
     
+    func getImageView() -> AnyView? {
+        guard let url = URL(string: story.coverImage) else {
+            return nil
+        }
+        
+
+        return AnyView(AsyncImage(url: url,
+                                  placeholder: Text("Loading ..."),
+                                  configuration: { $0.resizable() }).frame(width: 200, height: 200).cornerRadius(10)
+        )
+    }
+    
+    /**
+     if let, guard let and switch statements are not allowed within var body: some View
+     use a mehtod that returns AnyView as an alternative
+     
+     You are allowed to use if else inside if you you mark the view with @ViewBuilder as follows
+     
+     @ViewBuilder
+     var body: some View {
+         if user.isLoggedIn {
+             MainView()
+         } else {
+             LoginView()
+         }
+     }
+     */
     var body: some View {
         HStack {
             Spacer()
             VStack {
                 Text(story.title).font(.largeTitle).bold().foregroundColor(titleColor)
-                AsyncImage(
-                    url: URL(string: story.coverImage)!,
-                    placeholder: Text("Loading ..."), configuration: { $0.resizable() })
-                    .frame(width: 200, height: 200).cornerRadius(10)
+                getImageView()
                 Text(story.description)
                     .font(.footnote)
                     .fontWeight(.semibold)
